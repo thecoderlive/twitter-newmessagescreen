0f8e4c623d7c406eddbef61b27fb1e6a9bdd7ea4
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-01 16:29:19 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,35 @@
+export const item = {
+    "id": "f189d3db-55ad-475d-a5a7-bba8b05e8774",
+    "home_feed": [
+        {
+            "id": "9133d9b4-3803-41f5-983d-e6edcc8faffa",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png",
+            "user_name": "may",
+            "pseudonym": "common"
+        },
+        {
+            "id": "43fbba5a-be1d-4f23-a2cf-c1c669648d0e",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png",
+            "user_name": "fact",
+            "pseudonym": "speech"
+        },
+        {
+            "id": "d40f40b2-7dd2-464a-b42b-631ccf28dcec",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/251.png",
+            "user_name": "attention",
+            "pseudonym": "market"
+        },
+        {
+            "id": "252a6c1a-575f-4354-ab12-115d3e4a994a",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png",
+            "user_name": "visit",
+            "pseudonym": "daughter"
+        },
+        {
+            "id": "fb7d94da-034e-4dee-a9dd-1fcc64519fd3",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/60.png",
+            "user_name": "someone",
+            "pseudonym": "special"
+        }
+    ]
+}
